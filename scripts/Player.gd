6af2625fc1a3b948413697272c54c6fb10a07702
MintPaw <jerusanders@gmail.com>
2016-03-25 15:38:37 -0600

    Added basic Player script

---
+++ scripts/Player.gd
@@ -0,0 +1,15 @@
+# test
+extends Node
+
+var body
+var cam
+var updateE
+
+func _ready():
+	body = get_node("RigidBody")
+	cam = get_node("RigidBody/Camera")
+	set_process(true)
+	
+func _process(d):
+	print(d)
+	
